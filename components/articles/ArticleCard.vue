8cc2906b2020bb574fdada58b906d5c8e12f5054
Aziz Ridhwan Pratama <azizpratama@gnuweeb.org>
2023-10-12 23:13:41 +0700

    Tugas 5
    Signed-off-by: Aziz Ridhwan Pratama <azizpratama@gnuweeb.org>

---
+++ components/articles/ArticleCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    class="flex flex-col relative bg-slate-100 hover:shadow-2xl hover:bg-slate-50 p-4 md:p-7 rounded-2xl overflow-hidden duration-300 group">
+    <div class="relative w-full rounded-lg h-52 content-center overflow-hidden">
+      <img :src="article.img" class="object-hover group-hover:scale-110 transition">
+    </div>
+    <div>
+      <div class="font-bold text-slate-950 text-2xl leading-[1.5] my-4">{{ article.title }}</div>
+      <div class="text-slate-700 leading-[1.5] mb-5">{{ article.content }}</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import type { PropType } from 'vue'
+interface Article {
+  title: string,
+  content: string,
+  img: string
+}
+
+export default {
+  props: {
+    article: {
+      type: Object as PropType<Article>,
+      required: true
+    }
+  }
+}
+</script>
---
+++ pages/other/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="px-[20px] min-h-[100vh] md:px-[100px] xl:px-[150px] mb-[50px]">
+    <div class="mt-[90px] sm:mt-[100px] grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-8 content-stretch">
+      <ArticleCard v-for="article, idx in articles" :key="idx" :article="article" />
+    </div>
+  </div>
+</template>
+
+<script>
+import ArticleCard from '~/components/articles/ArticleCard.vue';
+
+export default {
+  data() {
+    return {
+      articles: [
+        {
+          title: "Judul Artikel 1",
+          content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat dolor explicabo sint quos exercitationem fugiat reiciendis assumenda. Ad quia veritatis, dolor odit, voluptas numquam modi porro eius reprehenderit ullam tenetur?",
+          img: 'https://images.unsplash.com/photo-1649282806617-c51bb282899c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
+        },
+        {
+          title: "Judul Artikel 2",
+          content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat dolor explicabo sint quos exercitationem fugiat reiciendis assumenda. Ad quia veritatis, dolor odit, voluptas numquam modi porro eius reprehenderit ullam tenetur?",
+          img: 'https://images.unsplash.com/photo-1635732646038-0a1a2fe3f2d6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80'
+        },
+        {
+          title: "Judul Artikel 3",
+          content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat dolor explicabo sint quos exercitationem fugiat reiciendis assumenda. Ad quia veritatis, dolor odit, voluptas numquam modi porro eius reprehenderit ullam tenetur?",
+          img: 'https://images.unsplash.com/photo-1640006807976-a6127e9d6fa0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80'
+        },
+      ]
+    }
+  },
+  components: {
+    ArticleCard
+  }
+}
+</script>
